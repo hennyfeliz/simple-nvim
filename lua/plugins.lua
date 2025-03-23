071f250ee8d2308602476800da2013e1f8a46831
henny <mingito.2017@gmail.com>
2025-03-23 01:59:47 -0400

    adding snacks, harpoon and nvim-treesitter plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -202,4 +202,260 @@ return require("lazy").setup({
 			},
 		},
 	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+
+			configs.setup({
+				ensure_installed = {
+					"javascript",
+					"typescript",
+					"c",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"elixir",
+					"erlang",
+					"heex",
+					"eex",
+					"kotlin",
+					"jq",
+					"dockerfile",
+					"json",
+					"html",
+					"terraform",
+					"go",
+					"tsx",
+					"bash",
+					"ruby",
+					"markdown",
+					"java",
+					"astro",
+				},
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<C-space>",
+						node_incremental = "<C-space>",
+						scope_incremental = "<C-CR>",
+						node_decremental = "<bs>",
+					},
+				},
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+						keymaps = {
+							-- You can use the capture groups defined in textobjects.scm
+							["aa"] = "@parameter.outer",
+							["ia"] = "@parameter.inner",
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+						},
+					},
+					move = {
+						enable = true,
+						set_jumps = true, -- whether to set jumps in the jumplist
+						goto_next_start = {
+							["]m"] = "@function.outer",
+							["]]"] = "@class.outer",
+						},
+						goto_next_end = {
+							["]M"] = "@function.outer",
+							["]["] = "@class.outer",
+						},
+						goto_previous_start = {
+							["[m"] = "@function.outer",
+							["[["] = "@class.outer",
+						},
+						goto_previous_end = {
+							["[M"] = "@function.outer",
+							["[]"] = "@class.outer",
+						},
+					},
+					swap = {
+						enable = true,
+						swap_next = {
+							["<leader>p"] = "@parameter.inner",
+						},
+						swap_previous = {
+							["<leader>ps"] = "@parameter.inner",
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		init = function()
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "OilActionsPost",
+				callback = function(event)
+					if event.data.actions.type == "move" then
+						Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+					end
+				end,
+			})
+		end,
+		keys = {
+			{
+				"<leader>bd",
+				function()
+					Snacks.bufdelete()
+				end,
+				desc = "Buffer delete",
+				mode = "n",
+			},
+			{
+				"<leader>ba",
+				function()
+					Snacks.bufdelete.all()
+				end,
+				desc = "Buffer delete all",
+				mode = "n",
+			},
+			{
+				"<leader>bo",
+				function()
+					Snacks.bufdelete.other()
+				end,
+				desc = "Buffer delete other",
+				mode = "n",
+			},
+			{
+				"<leader>bz",
+				function()
+					Snacks.zen()
+				end,
+				desc = "Toggle Zen Mode",
+				mode = "n",
+			},
+		},
+		opts = {
+			bigfile = { enabled = false },
+			dashboard = {
+				preset = {
+					pick = nil,
+					---@type snacks.dashboard.Item[]
+					keys = {
+						{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+						{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+						{ icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+						{ icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+						{
+							icon = " ",
+							key = "c",
+							desc = "Config",
+							action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+						},
+						{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
+						{ icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+						{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+					},
+					header = [[
+                                                                             
+               ████ ██████           █████      ██                     
+              ███████████             █████                             
+              █████████ ███████████████████ ███   ███████████   
+             █████████  ███    █████████████ █████ ██████████████   
+            █████████ ██████████ █████████ █████ █████ ████ █████   
+          ███████████ ███    ███ █████████ █████ █████ ████ █████  
+         ██████  █████████████████████ ████ █████ █████ ████ ██████ 
+      ]],
+				},
+				sections = {
+					{ section = "header" },
+					{
+						section = "keys",
+						indent = 1,
+						padding = 1,
+					},
+					{ section = "recent_files", icon = " ", title = "Recent Files", indent = 3, padding = 2 },
+					{ section = "startup" },
+				},
+			},
+			explorer = { enabled = false },
+			indent = { enabled = true },
+			input = { enabled = false },
+			picker = { enabled = false },
+			notifier = { enabled = false },
+			quickfile = { enabled = true },
+			scope = { enabled = false },
+			statuscolumn = { enabled = false },
+			words = { enabled = false },
+			rename = { enabled = true },
+			zen = {
+				enabled = true,
+				toggles = {
+					ufo = true,
+					dim = true,
+					git_signs = false,
+					diagnostics = false,
+					line_number = false,
+					relative_number = false,
+					signcolumn = "no",
+					indent = false,
+				},
+			},
+		},
+		config = function(_, opts)
+			require("snacks").setup(opts)
+
+			Snacks.toggle.new({
+				id = "ufo",
+				name = "Enable/Disable ufo",
+				get = function()
+					return require("ufo").inspect()
+				end,
+				set = function(state)
+					if state == nil then
+						require("noice").enable()
+						require("ufo").enable()
+						vim.o.foldenable = true
+						vim.o.foldcolumn = "1"
+					else
+						require("noice").disable()
+						require("ufo").disable()
+						vim.o.foldenable = false
+						vim.o.foldcolumn = "0"
+					end
+				end,
+			})
+		end,
+	},
+
+	{
+		"ThePrimeagen/harpoon",
+		config = function()
+			local mark = require("harpoon.mark")
+			local ui = require("harpoon.ui")
+
+			vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon: Mark File" })
+			vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Toggle Harpoon Menu" })
+
+			vim.keymap.set("n", "<C-t>", function()
+				ui.nav_file(1)
+			end, { desc = "Harpoon File 1" })
+			vim.keymap.set("n", "<C-s>", function()
+				ui.nav_file(2)
+			end, { desc = "Harpoon File 2" })
+			vim.keymap.set("n", "<C-b>", function()
+				ui.nav_file(3)
+			end, { desc = "Harpoon File 3" })
+			vim.keymap.set("n", "<C-g>", function()
+				ui.nav_file(4)
+			end, { desc = "Harpoon File 4" })
+		end,
+	},
 })
