74d4f357ad8c3ed1453248ca7bfcc2e28e777c1a
henny <mingito.2017@gmail.com>
2025-03-24 01:22:42 -0400

    adding plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -96,8 +96,8 @@ return require("lazy").setup({
     config = function()
       require("bufferline").setup({
         options = {
-          mode = "buffers",     -- this shows buffers, not tabpages
-          diagnostics = "nvim_lsp", -- optional: show LSP error indicators
+          mode = "buffers",          -- this shows buffers, not tabpages
+          diagnostics = "nvim_lsp",  -- optional: show LSP error indicators
           separator_style = "slant", -- or "thick", "thin", "padded_slant"
           always_show_bufferline = true,
           show_buffer_close_icons = false,
@@ -127,8 +127,10 @@ return require("lazy").setup({
         },
         formatters_by_ft = {
           lua = { "stylua" },
-          javascript = { "prettier" },
-          typescript = { "prettier" },
+          javascript = { "eslint_d", "prettier" },
+          typescript = { "eslint_d", "prettier" },
+          javascriptreact = { "eslint_d", "prettier" },
+          typescriptreact = { "eslint_d", "prettier" },
           json = { "prettier" },
           html = { "prettier" },
           css = { "prettier" },
@@ -140,6 +142,11 @@ return require("lazy").setup({
           java = { "google_java_format" },
         },
         formatters = {
+          eslint_d = {
+            command = "eslint_d",
+            args = { "--fix" },
+            stdin = false,
+          },
           prettier = {
             prepend_args = { "--tab-width", "2", "--use-tabs", "false" },
           },
@@ -212,6 +219,8 @@ return require("lazy").setup({
         ensure_installed = {
           "javascript",
           "typescript",
+          "tsx",
+          -- "jsx",
           "c",
           "lua",
           "vim",
@@ -462,6 +471,17 @@ return require("lazy").setup({
   {
     "neovim/nvim-lspconfig",
     dependencies = {
+      "stevearc/conform.nvim",
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/nvim-cmp",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "j-hui/fidget.nvim",
       "saghen/blink.cmp",
       {
         "folke/lazydev.nvim",
@@ -472,19 +492,70 @@ return require("lazy").setup({
         },
       },
     },
-    config = function()
-      local capabilities = require("blink.cmp").get_lsp_capabilities()
-      require("lspconfig").lua_ls.setup({ capabilites = capabilities })
 
+    config = function()
+      -- Setup formatters
+      require("conform").setup({ formatters_by_ft = {} })
+
+      -- LSP capabilities from blink.cmp
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+      -- Mason setup
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "rust_analyzer", "gopls" },
+        handlers = {
+          function(server_name)
+            require("lspconfig")[server_name].setup({ capabilities = capabilities })
+          end,
+
+          ["lua_ls"] = function()
+            require("lspconfig").lua_ls.setup({
+              capabilities = capabilities,
+              settings = {
+                Lua = {
+                  runtime = { version = "Lua 5.1" },
+                  diagnostics = {
+                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+                  },
+                },
+              },
+            })
+          end,
+
+          ["zls"] = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.zls.setup({
+              root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
+              settings = {
+                zls = {
+                  enable_inlay_hints = true,
+                  enable_snippets = true,
+                  warn_style = true,
+                },
+              },
+            })
+            vim.g.zig_fmt_parse_errors = 0
+            vim.g.zig_fmt_autosave = 0
+          end,
+        },
+      })
+
+      -- TypeScript custom cmd
+      require("lspconfig").tsserver.setup({
+        capabilities = capabilities,
+        cmd = {
+          "C:/Users/henny/scoop/apps/nodejs/current/bin/typescript-language-server.cmd",
+          "--stdio",
+        },
+      })
+
+      -- Auto format Lua on save
       vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
           local c = vim.lsp.get_client_by_id(args.data.client_id)
-          if not c then
-            return
-          end
-
+          if not c then return end
           if vim.bo.filetype == "lua" then
-            -- Format the current buffer on save
             vim.api.nvim_create_autocmd("BufWritePre", {
               buffer = args.buf,
               callback = function()
@@ -493,6 +564,193 @@ return require("lazy").setup({
             })
           end
         end,
+      })
+
+      -- cmp setup
+      local cmp = require("cmp")
+      local cmp_select = { behavior = cmp.SelectBehavior.Select }
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+          ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+
+      -- Diagnostics config
+      vim.diagnostic.config({
+        float = {
+          focusable = false,
+          style = "minimal",
+          border = "rounded",
+          source = "always",
+          header = "",
+          prefix = "",
+        },
+      })
+
+      -- Fidget UI
+      require("fidget").setup({})
+    end,
+  },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map("n", "]c", function()
+            if vim.wo.diff then
+              return "]c"
+            end
+            vim.schedule(function()
+              gs.next_hunk()
+            end)
+            return "<Ignore>"
+          end, { expr = true })
+
+          map("n", "[c", function()
+            if vim.wo.diff then
+              return "[c"
+            end
+            vim.schedule(function()
+              gs.prev_hunk()
+            end)
+            return "<Ignore>"
+          end, { expr = true })
+
+          -- Actions
+          map("n", "<leader>hs", gs.stage_hunk, { desc = "GitSigns state hunk" })
+          map("n", "<leader>hr", gs.reset_hunk, { desc = "GitSigns reset hunk" })
+          map("v", "<leader>hs", function()
+            gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+          end, { desc = "GitSigns stage_hunk" })
+          map("v", "<leader>hr", function()
+            gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+          end, { desc = "GitSigns reset_hunk" })
+          map("n", "<leader>hS", gs.stage_buffer, { desc = "GitSigns stage_buffer" })
+          map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "GitSigns undo_stage_hunk" })
+          map("n", "<leader>hR", gs.reset_buffer, { desc = "GitSigns reset_buffer" })
+          map("n", "<leader>hp", gs.preview_hunk, { desc = "GitSigns preview_hunk" })
+          map("n", "<leader>hb", function()
+            gs.blame_line({ full = true })
+          end, { desc = "GitSigns blame line" })
+          map("n", "<leader>htb", gs.toggle_current_line_blame, { desc = "GitSigns toggle blame" })
+          map("n", "<leader>hd", gs.diffthis, { desc = "GitSigns diffthis" })
+          map("n", "<leader>hD", function()
+            gs.diffthis("~")
+          end, { desc = "GitSigns diffthis" })
+          map("n", "<leader>htd", gs.toggle_deleted, { desc = "GitSigns toggle_deleted" })
+
+          -- Text object
+          map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "GitSigns select hunk" })
+        end,
+      })
+    end,
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+
+  -- Autocomplete core
+  { "hrsh7th/nvim-cmp" },
+  -- LSP source
+  { "hrsh7th/cmp-nvim-lsp" },
+  -- Snippet engine
+  { "L3MON4D3/LuaSnip" },
+  -- Snippets source
+  { "saadparwaiz1/cmp_luasnip" },
+  -- Path and buffer completions
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-buffer" },
+
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    config = function()
+      require("onedark").setup({
+        -- Your config goes here
+        style = "darker",
+        transparent = false,
+        term_colors = true,
+        ending_tildes = false,
+        cmp_itemkind_reverse = false,
+        toggle_style_key = nil,
+        toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" },
+        code_style = {
+          comments = "italic",
+          keywords = "none",
+          functions = "none",
+          strings = "none",
+          variables = "none",
+        },
+        lualine = {
+          transparent = false,
+        },
+        colors = {},
+        highlights = {},
+        diagnostics = {
+          darker = true,
+          undercurl = true,
+          background = true,
+        },
+      })
+      require("onedark").load()
+    end,
+  },
+
+  {
+    "nvim-treesitter/playground",
+    cmd = "TSPlaygroundToggle",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        playground = { enable = true },
+      })
+    end,
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          enabled = true,
+          auto_trigger = false,
+          hide_during_completion = false,
+          debounce = 25,
+          keymap = {
+            accept = false,
+            accept_word = false,
+            accept_line = "<Tab>",
+            next = false,
+            prev = false,
+            dismiss = false,
+          },
+        },
       })
     end,
   },
