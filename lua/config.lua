6ec4de0f7b7201195ace668efcc0ed734d35fd3f
henny <mingito.2017@gmail.com>
2025-03-24 01:22:25 -0400

    treesitter, cmp, onedark theme config and more...

--- lua/config.lua
+++ lua/config.lua
@@ -1,4 +1,10 @@
+vim.cmd.colorscheme("onedark")
+local cmp = require("cmp")
 local actions = require("telescope.actions")
+local status, lualine = pcall(require, "lualine")
+if not status then
+	return
+end
 
 -- Configuración de Telescope...
 require("telescope").setup({
@@ -67,4 +73,79 @@ require("nvim-web-devicons").setup({
 			name = "Apple",
 		},
 	},
+
+	lualine.setup({
+		options = {
+			icons_enabled = true,
+			theme = "onedark",
+			section_separators = { left = "", right = "" },
+			component_separators = { left = "", right = "" },
+			disabled_filetypes = {},
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch" },
+			lualine_c = {
+				{
+					"filename",
+					file_status = true, -- displays file status (readonly status, modified status)
+					path = 0, -- 0 = just filename, 1 = relative path, 2 = absolute path
+				},
+			},
+			lualine_x = {
+				{
+					"diagnostics",
+					sources = { "nvim_diagnostic" },
+					symbols = { error = " ", warn = " ", info = " ", hint = " " },
+				},
+				"encoding",
+				"filetype",
+			},
+			lualine_y = { "progress" },
+			lualine_z = { "location" },
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = {
+				{
+					"filename",
+					file_status = true, -- displays file status (readonly status, modified status)
+					path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+				},
+			},
+			lualine_x = { "location" },
+			lualine_y = {},
+			lualine_z = {},
+		},
+		tabline = {},
+		extensions = { "fugitive" },
+	}),
+
+	cmp.setup({
+		snippet = {
+			expand = function(args)
+				require("luasnip").lsp_expand(args.body)
+			end,
+		},
+		sources = {
+			{ name = "nvim_lsp" },
+			{ name = "buffer" },
+			{ name = "path" },
+			{ name = "luasnip" },
+		},
+		mapping = cmp.mapping.preset.insert({
+			["<Tab>"] = cmp.mapping.select_next_item(),
+			["<S-Tab>"] = cmp.mapping.select_prev_item(),
+			["<CR>"] = cmp.mapping.confirm({ select = true }),
+		}),
+	}),
+
+	vim.filetype.add({
+		extension = {
+			jsx = "javascriptreact",
+		},
+	}),
+
+	vim.treesitter.language.register("tsx", "javascriptreact"),
 })
