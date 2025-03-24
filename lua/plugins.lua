69f1f14f31bc307973edf4823e633590b4157ada
henny <mingito.2017@gmail.com>
2025-03-24 14:23:57 -0400

    adding markview for reading markdown files and see hightlighted things on development how it would be

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -799,4 +799,14 @@ return require("lazy").setup({
       })
     end,
   },
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+
+    -- For blink.cmp's completion
+    -- source
+    dependencies = {
+      "saghen/blink.cmp"
+    },
+  },
 })
