f99e677d7f37fe72e949bfd610f023f621fb584d
henny <mingito.2017@gmail.com>
2025-03-24 01:21:00 -0400

    path config and adding onedark theme config

--- init.lua
+++ init.lua
@@ -1,20 +1,22 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+vim.env.PATH = "C:/Users/henny/scoop/persist/nodejs/bin;" .. vim.env.PATH
+
 -- ~/.config/nvim/init.lua
 
 -- 1. Bootstrap lazy.nvim (the plugin manager)
 --    This automatically clones lazy.nvim to your system if it’s not present.
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -22,7 +24,8 @@ vim.opt.rtp:prepend(lazypath)
 require("plugins")
 require("keymaps")
 require("config")
-require("java")
+require("theme")
+
 
 -- 3. (Optional) Set some basic options or keymaps here
 --    e.g.:
