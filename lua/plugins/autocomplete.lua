5287566d00f424a5f54e25ebb9b78b923a07d9e1
henny <mingito.2017@gmail.com>
2025-03-27 16:03:21 -0400

    autocomplete.lua pluging file

---
+++ lua/plugins/autocomplete.lua
@@ -0,0 +1,13 @@
+return {
+  -- Autocomplete core
+  { "hrsh7th/nvim-cmp" },
+  -- LSP source
+  { "hrsh7th/cmp-nvim-lsp" },
+  -- Snippet engine
+  { "L3MON4D3/LuaSnip" },
+  -- Snippets source
+  { "saadparwaiz1/cmp_luasnip" },
+  -- Path and buffer completions
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-buffer" },
+}
