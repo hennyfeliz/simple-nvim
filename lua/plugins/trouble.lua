63b8fc2492c8a96feb96b91e86ec3be53a921ad0
henny <mingito.2017@gmail.com>
2025-03-28 23:52:28 -0400

    trouble.lua plugin file

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,52 @@
+-- truble for errors and warnings debugging
+return {
+  "folke/trouble.nvim",
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+    {
+      "<leader>xn",
+      function()
+        require("trouble").next({ skip_groups = true, jump = true })
+      end,
+      desc = "Next Trouble Item",
+    },
+    {
+      "<leader>xp",
+      function()
+        require("trouble").previous({ skip_groups = true, jump = true })
+      end,
+      desc = "Previous Trouble Item",
+    },
+  },
+}
