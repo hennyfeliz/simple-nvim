62dd276a4d166816f042696e6e5c3329bbb61e9b
henny <mingito.2017@gmail.com>
2025-05-14 21:17:36 -0500

    nvim-web-devicons.lua config file changes, adding some more config

--- lua/plugins/nvim-web-devicons.lua
+++ lua/plugins/nvim-web-devicons.lua
@@ -1,2 +1,18 @@
 -- nvim web devicons
-return { "nvim-tree/nvim-web-devicons", opts = {} }
+return {
+  "kyazdani42/nvim-web-devicons",
+  config = function()
+    require("nvim-web-devicons").setup {
+      -- enable “override” for custom files
+      override = {
+        -- example: markdown files get the Markdown icon
+        md = {
+          icon = "",
+          color = "#519aba",
+          name = "Markdown"
+        },
+      },
+      default = true,
+    }
+  end,
+}
