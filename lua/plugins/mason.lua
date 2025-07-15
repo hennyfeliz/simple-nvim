a6a1f1a1bb6056eec4d590ba061c0783682e2da2
henny <mingito.2017@gmail.com>
2025-07-14 22:51:48 -0400

    mason.lua config file changes

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -6,6 +6,26 @@ return {
     { "<leader>m", "<cmd>Mason<CR>", desc = "Open Mason" },
   },
   config = function()
-    require("mason").setup()
+    require("mason").setup({
+      ensure_installed = {
+        -- LSP servers
+        "jdtls",              -- Java Language Server
+        "ts_ls",              -- TypeScript/JavaScript LSP
+        "eslint",             -- ESLint LSP
+        "html",               -- HTML LSP
+        "cssls",              -- CSS LSP
+        "jsonls",             -- JSON LSP
+        "lua_ls",             -- Lua LSP
+        
+        -- Formatters
+        "prettier",           -- JavaScript/TypeScript/HTML/CSS formatter
+        "stylua",             -- Lua formatter
+        "black",              -- Python formatter
+        "clang_format",       -- C/C++ formatter
+        "shfmt",              -- Shell formatter
+        "rustfmt",            -- Rust formatter
+      },
+      automatic_installation = true,
+    })
   end,
 }
