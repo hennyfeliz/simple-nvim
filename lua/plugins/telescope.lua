e4c687e158ad7f74a2d2343d492da5aaf2067b44
henny <mingito.2017@gmail.com>
2025-07-28 23:57:02 -0400

    telescope changes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,15 +2,33 @@
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("telescope").setup({
-      defaults = {
-        prompt_prefix = "🔍 ",
-        selection_caret = "➜ ",
-        path_display = { "smart" },
-      },
-      -- pickers = { ... } -- optional per-picker configs
-      -- extensions = { ... } -- optional extension configs
+  opts = function(_, opts)
+    local ivy = require("telescope.themes").get_ivy({
+      layout_config = { height = 0.30, preview_width = 0.7 },
+      -- remove titles
+      results_title = false,
+      preview_title = false,
     })
+
+    -- custom border: only a vertical line between results & preview
+    ivy.borderchars = {
+      prompt = { " ", " ", " ", " ", " ", " ", " ", " " },
+      results = { " ", " ", " ", " ", " ", " ", " ", " " },
+      preview = { " ", "│", " ", " ", " ", " ", " ", " " },
+    }
+
+    -- custom helper to show only parentdir/filename
+    local function parentdir_filename(_, path)
+      local fname = vim.fn.fnamemodify(path, ':t')        -- filename
+      local parent = vim.fn.fnamemodify(path, ':h:t')     -- parent dir tail
+      if parent == '.' or parent == '' then
+        return fname
+      end
+      return parent .. '/' .. fname
+    end
+
+    ivy.path_display = parentdir_filename
+
+    opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, ivy)
   end,
 }
